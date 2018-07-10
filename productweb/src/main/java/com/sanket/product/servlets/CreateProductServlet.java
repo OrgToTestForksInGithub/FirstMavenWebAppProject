d1f26f09125dcfefef5096685b2f1178d4f9695e
Sanket Kini <savage.monk@gmail.com>
2018-07-10 23:57:53 +0100

    Working Project

--- productweb/src/main/java/com/sanket/product/servlets/CreateProductServlet.java
+++ productweb/src/main/java/com/sanket/product/servlets/CreateProductServlet.java
@@ -1,10 +1,19 @@
 package com.sanket.product.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.sanket.product.bo.ProductBO;
+import com.sanket.product.dto.Product;
+
 
 /**
  * Servlet implementation class CreateProductServlet
@@ -13,7 +22,23 @@ public class CreateProductServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
    	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
+   		int id= Integer.parseInt(request.getParameter("id"));
+   		String name = request.getParameter("name");
+   		String description = request.getParameter("description");
+   		int price= Integer.parseInt(request.getParameter("price"));
+   		
+   		Product product = new Product();
+   		product.setId(id);
+   		product.setName(name);
+   		product.setDescription(description);
+   		product.setPrice(price);
+   		
+   		WebApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext(this.getServletContext());
+   		ProductBO bo = (ProductBO) context.getBean("productbo");
+   		bo.createProduct(product);
+   		
+   		PrintWriter output = response.getWriter();
+   		output.print("Product Created!!!");
+  	}
 
 }
